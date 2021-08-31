cf96639cbf5151632f2bf53dbe5945207a3fe98c
João Hugo Leda <joaohugoleda@gmail.com>
2021-08-30 22:36:33 -0300

    .

--- Matrizes/01.py
+++ Matrizes/01.py
@@ -9,18 +9,48 @@ e) O procedimento deve retornar a matriz alterada.
 
 import random
 
-def alterarMatriz(a: list):
+def linha2para8(a: list):
   linha2 = []
   linha8 = []
+
   for i in range(10):
     linha2.append(a[1][i])
     linha8.append(a[7][i])
+
   for i in range(10):
     a[1][i] = linha8[i]
     a[7][i] = linha2[i]
+  
+  return a
 
-a = [0] * 10
-for i in a:
-  a.append([0] * 10)
+def mudarDuasColunas(a: list, c1: int, c2: int):
+  colx = []
+  coly = []
 
-print(a)
+  for i in range(10):
+    colx.append(a[i][c1 - 1])
+    coly.append(a[i][c2 - 1])
+
+  for i in range(10):
+    a[i][c1 - 1] = colx[i]
+    a[i][c2 - 1] = coly[i]
+  
+  return a
+
+def alterarMatriz(a: list):
+  a = linha2para8(a)
+  #a = mudarDuasColunas(a, 4, 10)
+
+def printarMatrizLinhas(matriz):
+  for i in range(len(matriz)):
+    print(matriz[i])
+
+matrizA = [random.randint(0, 100)] * 10
+
+for i in range(10):
+    matrizA[i] = [random.randint(0, 100)] * 10
+
+printarMatrizLinhas(matrizA)
+matrizA = alterarMatriz(matrizA)
+print('')
+printarMatrizLinhas(matrizA)
