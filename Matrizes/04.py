a123996d920830907a65ff321c284c83c30471ce
João Hugo Leda <74215171+HugoLeda@users.noreply.github.com>
2021-09-01 21:34:33 -0300

    Update 04.py

--- Matrizes/04.py
+++ Matrizes/04.py
@@ -1,1 +1,3 @@
 #  Faça um procedimento que receba por parâmetro, uma matriz A(6,6) e multiplica cada linha pelo elemento da diagonal principal daquela linha. O procedimento deve retornar a matriz alterada.
+
+def manipularMatriz(a: list):
