33febece00a5113298161c2ff03089bc73732aa4
João Hugo Leda <joaohugoleda@gmail.com>
2021-09-05 18:25:18 -0300

    vetores - pares

--- Vetores/03.py
+++ Vetores/03.py
@@ -1,1 +1,18 @@
 # Escreva  um  algoritmo  que  leia  e  mostre  um  vetor  de  20  elementos inteiros.  a  seguir,  conte  quantos  valores pares existem no vetor.
+
+def qtdPares(v: list):
+  pares = 0
+  for i in v:
+    if ((i % 2) == 0):
+      pares += 1
+  return pares
+
+def lerVetor(p: int):
+  res = []
+  for i in range(p):
+    n = float(input('Digite um número: '))
+    res.append(n)
+  return res
+
+lista = lerVetor(5)
+print(f'A quantidade de números pares na lista é: {qtdPares(lista)}')
--- Vetores/11.py
+++ Vetores/11.py
@@ -1,1 +1,1 @@
-# Escrever um algoritmo que lê um vetor X(20) e o escreve. Escreva, a seguir, cada um dos valores distintos que aparecem em X dizendo quantas vezes cada valor aparece em X. 
+# Escrever um algoritmo que lê um vetor X(20) e o escreve. Escreva, a seguir, cada um dos valores distintos que aparecem em X dizendo quantas vezes cada valor aparece em X.
