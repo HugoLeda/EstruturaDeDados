9c32d7f0390e318491c964aef4608bcfb619b203
João Hugo Leda <joaohugoleda@gmail.com>
2021-10-19 21:35:53 -0300

    grafo 1

---
+++ grafos/grafo.py
@@ -0,0 +1,33 @@
+grafo = [
+  {
+    "vertice": 'a',
+    "arestas": ['c', 'd', 'f']
+  },
+  {
+    "vertice": 'b',
+    "arestas": ['d', 'e']
+  },
+  {
+    "vertice": 'c',
+    "arestas": ['a', 'f']
+  },
+  {
+    "vertice": 'd',
+    "arestas": ['a', 'b', 'e', 'f']
+  },
+  {
+    "vertice": 'e',
+    "arestas": ['b', 'd']
+  },
+  {
+    "vertice": 'f',
+    "arestas": ['a', 'c', 'd']
+  },
+  {
+    "vertice": 'g',
+    "arestas": []
+  }
+]
+
+for i in grafo:
+  print(i)
