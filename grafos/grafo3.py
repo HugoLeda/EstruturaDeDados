8ff657ed87451161f3bd418669d62c7a7c561c84
João Hugo Leda <joaohugoleda2@gmail.com>
2021-10-25 19:41:36 -0300

    grafo3

---
+++ grafos/grafo3.py
@@ -0,0 +1,248 @@
+grafo3 = [{
+  "a": [
+  {
+    "aresta": "ac",
+    "incidencia": 1
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 1
+  },
+  {
+    "aresta": "af",
+    "incidencia": 1
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 0
+  },
+  {
+    "aresta": "be",
+    "incidencia": 0
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 0
+  },
+  {
+    "aresta": "de",
+    "incidencia": 0
+  },
+  {
+    "aresta": "df",
+    "incidencia": 0
+  }],
+}, 
+{
+  "b": [
+  {
+    "aresta": "ac",
+    "incidencia": 0
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 0
+  },
+  {
+    "aresta": "af",
+    "incidencia": 0
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 1
+  },
+  {
+    "aresta": "be",
+    "incidencia": 1
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 0
+  },
+  {
+    "aresta": "de",
+    "incidencia": 0
+  },
+  {
+    "aresta": "df",
+    "incidencia": 0
+  }],
+},
+{
+  "c": [
+  {
+    "aresta": "ac",
+    "incidencia": 1
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 0
+  },
+  {
+    "aresta": "af",
+    "incidencia": 0
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 0
+  },
+  {
+    "aresta": "be",
+    "incidencia": 0
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 1
+  },
+  {
+    "aresta": "de",
+    "incidencia": 0
+  },
+  {
+    "aresta": "df",
+    "incidencia": 0
+  }],
+},
+{
+  "d": [
+  {
+    "aresta": "ac",
+    "incidencia": 0
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 1
+  },
+  {
+    "aresta": "af",
+    "incidencia": 0
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 1
+  },
+  {
+    "aresta": "be",
+    "incidencia": 0
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 0
+  },
+  {
+    "aresta": "de",
+    "incidencia": 1
+  },
+  {
+    "aresta": "df",
+    "incidencia": 1
+  }],
+},
+{
+  "e": [
+  {
+    "aresta": "ac",
+    "incidencia": 0
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 0
+  },
+  {
+    "aresta": "af",
+    "incidencia": 0
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 0
+  },
+  {
+    "aresta": "be",
+    "incidencia": 1
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 0
+  },
+  {
+    "aresta": "de",
+    "incidencia": 1
+  },
+  {
+    "aresta": "df",
+    "incidencia": 0
+  }],
+},
+{
+  "f": [
+  {
+    "aresta": "ac",
+    "incidencia": 0
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 0
+  },
+  {
+    "aresta": "af",
+    "incidencia": 1
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 0
+  },
+  {
+    "aresta": "be",
+    "incidencia": 0
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 1
+  },
+  {
+    "aresta": "de",
+    "incidencia": 0
+  },
+  {
+    "aresta": "df",
+    "incidencia": 1
+  }],
+},
+{
+  "g": [
+  {
+    "aresta": "ac",
+    "incidencia": 0
+  }, 
+  {
+    "aresta": "ad",
+    "incidencia": 0
+  },
+  {
+    "aresta": "af",
+    "incidencia": 0
+  },
+  {
+    "aresta": "bd",
+    "incidencia": 0
+  },
+  {
+    "aresta": "be",
+    "incidencia": 0
+  },
+  {
+    "aresta": "cf",
+    "incidencia": 0
+  },
+  {
+    "aresta": "de",
+    "incidencia": 0
+  },
+  {
+    "aresta": "df",
+    "incidencia": 0
+  }],
+},]
+
+for i in range(len(grafo3)):
+  print(grafo3[i])
